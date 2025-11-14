cc1a72cd5a99f7fd7be7991769157c2362c1442c
Monika Yordanova <122971736+Mon0220@users.noreply.github.com>
2025-11-14 22:57:06 +0000

    Add files via upload

---
+++ 1_Coding_is_cool.cpp
@@ -0,0 +1,8 @@
+// print_message.cpp
+#include <iostream> // include the input-output stream library
+using namespace std; // include the standard input-output stream library
+
+int main() {   // main function
+    cout << "Coding is Cool!" << endl; // print the message
+    return 0; 
+}
